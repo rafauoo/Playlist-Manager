f4e08c5bb0999b6283091ee042bcefd80638c7fd
Rafal <rafal5094@gmail.com>
2022-03-23 15:59:16 +0100

    Added argument parser function

--- src/sources/main.cpp
+++ src/sources/main.cpp
@@ -3,7 +3,7 @@
 #include "interface.h"
 using namespace std;
 
-void call_interface(Playlist p)
+void call_interface(Playlist &p)
 {
     Interface ui(p);
     while (ui.breaks == false)
@@ -15,47 +15,49 @@ void call_interface(Playlist p)
     }
 }
 
+void argparse(Playlist &p, int argc, char* argv[])
+{
+    if (argc > 7)
+    {
+        for (int i = 7; i < argc; i++)
+        {
+            p.add_song(argv[i]);
+        }
+    }
+    if (argc > 6)
+    {
+        p.set_play_type(argv[6]);
+    }
+    if (argc > 5)
+    {
+        p.set_duration(stoi(argv[5]));
+    }
+    if (argc > 4)
+    {
+        p.set_date_modified(argv[4]);
+    }
+    if (argc > 3)
+    {
+        p.set_date_created(argv[3]);
+    }
+    if (argc > 2)
+    {
+        p.set_author(argv[2]);
+    }
+    if (argc > 1)
+    {
+        cout << argv[1];
+        p.set_name(argv[1]);
+    }
+}
+
 int main (int argc, char* argv[])
 {
-    if (argc == 1)
-    {
-        Playlist plds;
-        call_interface(plds);
-    }
+    Playlist plds;
+    if (argc == 1) call_interface(plds);
     else
     {
-        Playlist plds;
-        if (argc > 7)
-        {
-            for (int i = 7; i < argc; i++)
-            {
-                plds.add_song(argv[i]);
-            }
-        }
-        if (argc > 6)
-        {
-            plds.set_play_type(argv[6]);
-        }
-        if (argc > 5)
-        {
-            plds.set_duration(stoi(argv[5]));
-        }
-        if (argc > 4)
-        {
-            plds.set_date_modified(argv[4]);
-        }
-        if (argc > 3)
-        {
-            plds.set_date_created(argv[3]);
-        }
-        if (argc > 2)
-        {
-            plds.set_author(argv[2]);
-        }
-        if (argc > 1)
-        {
-            plds.set_name(argv[1]);
-        }
+        argparse(plds, argc, argv);
         call_interface(plds);
     }
     return 0;
